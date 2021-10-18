018b275c283a4b61ec4f43d8903d0ada11dc3077
Fatih20 <fatih@ThinkPad-X230-1b1323db.localdomain>
2021-10-18 09:14:35 +0700

    Initial commit

---
+++ Program.py
@@ -0,0 +1,54 @@
+gerbang_tol_list = [["Jakarta", 200], ["Tegal", 50], ["Semarang", 100], ["Jogjakarta", 100], ["Surabaya", 200]]
+ 
+print("Mau masuk gerbang tol mana?")
+ 
+i = 0
+for gerbang_tol in gerbang_tol_list:
+    print(f"{i+1} {gerbang_tol[0]}")
+    i+=1
+print("")
+posisi = int(input(""))-1
+print("")
+ 
+posisi_penghenti = [0, len(gerbang_tol_list)-1]
+ 
+if posisi == len(gerbang_tol_list)-1:
+    go_left = True
+    # posisi_penghenti.remove(posisi_penghenti[1])
+elif posisi == 0:
+    go_left = False
+    # posisi_penghenti.remove(posisi_penghenti[0])
+else :
+    arah = input("Mau ke arah Timur atau Barat? ").lower()
+    print("")
+    if arah == "timur":
+        go_left = False
+    elif arah == "barat":
+        go_left = True
+ 
+tarif = 0
+# tarif += gerbang_tol_list[posisi][1]
+ 
+if go_left:
+    pengubah_posisi = -1
+else :
+    pengubah_posisi = 1
+ 
+posisi += pengubah_posisi
+ 
+ujung_tol = True
+while posisi not in posisi_penghenti:
+    print(f"Anda sekarang berada di gerbang tol {gerbang_tol_list[posisi][0]}. Apakah anda akan melanjutkan perjalanan atau keluar di sini? keluar/lanjut")
+    keputusan = input().lower()
+    print("")
+    tarif += gerbang_tol_list[posisi][1]
+    if keputusan == "keluar":
+        ujung_tol = False
+        break
+    elif keputusan == "lanjut":
+        posisi += pengubah_posisi
+ 
+if ujung_tol:
+    tarif += gerbang_tol_list[posisi][1]
+ 
+print(f"Anda sekarang telah sampai di kota {gerbang_tol_list[posisi][0]}. Terimakasih telah menggunakan tol Jasamarga. Saldo sebesar {tarif} telah dipotong dari kartu tol anda.")
